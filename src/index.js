c9393630a26d81b6feb1d851fd87033f22aa1f96
Sam Parsons <samparsons269@gmail.com>
2021-03-18 23:23:33 -0400

    first draft of walrus operator in js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
-import { types as t } from '@babel/core';
+import { types as t, template } from '@babel/core';
+
+const wrapper = template(`
+    (function(IDENTIFIER) {
+      if(typeof IDENTIFIER === 'undefined') throw new Error();
+      IDENTIFIER = VALUE;
+      return IDENTIFIER;
+    })(IDENTIFIER);
+`);
 
 export default function transformWalrusOperator() {
   return {
     name: 'transform-walrus-operator',
     visitor: {
       AssignmentExpression(path) {
-        if (path.node.operator === ':=') console.log('here');
+        if (path.node.operator !== ':=') return;
+
+        const IDENTIFIER = path.node.left.name;
+        const VALUE = path.node.right;
+
+        // Get the iife wrapper Node
+        const wrappedBody = wrapper({
+          IDENTIFIER,
+          VALUE,
+        });
+
+        path.replaceWith(wrappedBody);
+
+        return;
       },
     },
   };
